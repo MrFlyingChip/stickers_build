5c2fc8f32f4081dc8b104c74928e2637788a393e
Kirill Minaev <kv_l@ukr.net>
2018-12-20 00:31:26 +0200

    build

---
+++ precache-manifest.893587e737854a8ce56dc30b0e01bbd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ff0118cf9e0f7041357",
+    "url": "/static/js/main.3ff0118c.chunk.js"
+  },
+  {
+    "revision": "50302e6d68e341ac52c1",
+    "url": "/static/js/1.50302e6d.chunk.js"
+  },
+  {
+    "revision": "3ff0118cf9e0f7041357",
+    "url": "/static/css/main.0db091d6.chunk.css"
+  },
+  {
+    "revision": "bb7abb26c1c37c516f933f32701d0a0f",
+    "url": "/index.html"
+  }
+];
